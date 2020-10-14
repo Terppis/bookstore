3a1ec0282cc1895d964fa9f28110f6185e371e47
Terppis <kristian.teemu@gmail.com>
2020-10-15 02:37:36 +0300

    Added user database and more

--- src/main/java/swd/bookstore/WebSecurityConfig.java
+++ src/main/java/swd/bookstore/WebSecurityConfig.java
@@ -1,23 +1,27 @@
 package swd.bookstore;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import swd.bookstore.domain.User;
+import swd.bookstore.domain.UserRepository;
+import swd.bookstore.web.UserDetailServiceImpl;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {	
 	
+	@Autowired UserDetailServiceImpl userDetailsService;
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 	return new BCryptPasswordEncoder();
@@ -27,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
         .authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
-        .antMatchers("/delete/{id}").hasRole("ADMIN")
+        //.antMatchers("/delete/{id}").hasAuthority("ADMIN")
         .and()
         .authorizeRequests()
         .anyRequest().authenticated()
@@ -40,23 +44,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
           .permitAll();
     }
     
-    
     @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        UserDetails user1 = User
-                .withUsername("user")
-                .password("$2a$10$zNp1fFsu2WCqTfAlvbNcg.PX7/xZVYTj2Khd9QPO8/fuz9hBLGYIC")
-                .roles("USER")
-                .build();
-        UserDetails user2 = User
-                .withUsername("admin")
-                .password("$2a$10$98WpMIwDT1Q1hJDuoC7EXu3lTyUeyKjXDuwJXaA3EoNo/Es4zlwLG")
-                .roles("ADMIN")
-                .build();      
-         
-        return new InMemoryUserDetailsManager(user1, user2);
+	public CommandLineRunner userdemo(UserRepository userRepository) {
+		return (args) -> {
+			
+			User user1= new User("teemu", "$2a$10$98WpMIwDT1Q1hJDuoC7EXu3lTyUeyKjXDuwJXaA3EoNo/Es4zlwLG", "testi.teemu@gmail.com", "ADMIN");
+			User user2= new User("matti", "$2a$10$o7LOKuTBLObZuqX/a55ZLeWaDzvOTjRvI2tr8w7mc6ke89PvsBfme" ,"testi.matti@gmail.com", "USER");
+			User user3= new User("liisa", "$2a$10$.PZPUSOruRZ2zxvcJ5yBZ.d4ABn/SqPfGFMpT2mnmdHkbhAF5D.02", "testi.liisa@gmail.com", "USER");
+			userRepository.save(user1);
+			userRepository.save(user2);
+			userRepository.save(user3); 
+				
+		};
     }
     
-    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 }
---
+++ src/main/java/swd/bookstore/domain/UserRepository.java
@@ -0,0 +1,9 @@
+package swd.bookstore.domain;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	User findByUsername(String username);
+}
