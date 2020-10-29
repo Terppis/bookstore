02683d548700b47e9f7416e9e206e96c16cf817a
Terppis <kristian.teemu@gmail.com>
2020-10-29 12:23:05 +0200

    fixes to secConfig

--- src/main/java/swd/bookstore/WebSecurityConfig.java
+++ src/main/java/swd/bookstore/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated()
         .and()
       .formLogin()
-          .defaultSuccessUrl("/booklist")
+          .defaultSuccessUrl("/booklist", true)
           .permitAll()
           .and()
       .logout()
